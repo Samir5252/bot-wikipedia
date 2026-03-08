f74ac1b46cd7fe280ee0353b67f8e2aea65c039d
Samir5252 <gabrielsamir525@gmail.com>
2026-03-08 03:18:12 -0600

    Add automated search and time sleeps

--- bot_wiki.py
+++ bot_wiki.py
@@ -1,1 +1,12 @@
-a
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import time
+navegador = webdriver.Edge()
+navegador.get("https://wikipedia.org")
+
+barra_busqueda = navegador.find_element(By.NAME, "search")
+barra_busqueda.send_keys("AWS")
+barra_busqueda.send_keys(Keys.RETURN)
+time.sleep(10)
