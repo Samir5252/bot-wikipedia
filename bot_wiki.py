0c4d44d4c148a6fa6ad1c66cb0a55fb4ce5f4a2f
Samir <gabrielsamir525@gmail.com>
2026-03-09 09:20:46 -0600

    Final code with screenshot

--- bot_wiki.py
+++ bot_wiki.py
@@ -3,10 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 import time
-navegador = webdriver.Edge()
+navegador = webdriver.Firefox()
 navegador.get("https://wikipedia.org")
 
 barra_busqueda = navegador.find_element(By.NAME, "search")
-barra_busqueda.send_keys("AWS")
+barra_busqueda.send_keys("Dungeons & dragons")
 barra_busqueda.send_keys(Keys.RETURN)
-time.sleep(10)
+time.sleep(2)
+titulo = navegador.find_element(By.ID,"firstHeading")
+print("El bot encontro el articulo", titulo.text)
+navegador.save_screenshot("Evidencia.png")
+print("Se tomo una screenshot del articulo", titulo.text)
+navegador.quit()
